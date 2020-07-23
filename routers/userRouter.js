54735d019ba18add947bbeedb484ea65dcb952c0
SSJ <canwefly89@gmail.com>
2020-07-23 19:32:51 +0900

    ~2.11 MVC part done

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,13 @@
 import express from "express";
+import routes from "../routes";
+import { users, userDetail, editProfile, changeProfile } from "../controllers/userControllers";
 
-export const userRouter = express.Router();
+const userRouter = express.Router();
+
+userRouter.get(routes.users, users)
+userRouter.get(routes.userDetail, userDetail)
+userRouter.get(routes.editProfile, editProfile)
+userRouter.get(routes.changePassword, changeProfile)
 
 export default userRouter;
 
---
+++ routes.js
@@ -0,0 +1,39 @@
+// Glbal
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+// Users
+const USERS = "/users";
+const USER_DETAIL = "/:id";  // 위 두개를 혼합하면 users/1 이런 식으로 id 부여
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
+
+// Videos
+const VIDEOS = "/videos";
+const UPLOAD = "/upload";
+const VIDEO_DETAIL = "/:id";   // express에서 :id 같은건 변하는 값인걸 알아챔
+const EDIT_VIDEO = "/:id/edit";
+const DELETE_VIDEO = "/:id/delete";
+
+// make an object
+const routes = {
+  home: HOME, 
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: VIDEO_DETAIL,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO
+}
+
+export default routes;
