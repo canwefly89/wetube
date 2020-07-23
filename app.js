4a957545ee9e3d409a928a84beb5d9c7ae5f2375
SSJ <canwefly89@gmail.com>
2020-07-23 17:31:28 +0900

    2.9 MVC router setting fin

--- app.js
+++ app.js
@@ -5,14 +5,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
-
-
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from Home ass");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -20,10 +16,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/",  handleHome);  
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
-app.get("/profile",  handleProfile);
-
-app.use("/user", userRouter);  // router.js로부터 import
 
 export default app;
--- router.js
+++
@@ -1,7 +0,0 @@
-import express, { Router } from "express";
-
-export const userRouter = express.Router();
-
-userRouter.get("/", (req, res) => res.send('user index'));  // home에다가 함수 하나 넣기
-userRouter.get("/edit", (req, res) => res.send('user edit'));
-userRouter.get("/password", (req, res) => res.send('user password'));
